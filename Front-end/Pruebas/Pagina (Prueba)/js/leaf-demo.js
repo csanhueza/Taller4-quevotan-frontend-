c40378c99f281542966e3148757925050e1cfbb9
Marcelo Castillo <44004033+MarceloCastillo98@users.noreply.github.com>
2018-11-04 23:48:44 -0300

    mapa con mas funciones

---
+++ Front-end/Pruebas/Pagina (Prueba)/js/leaf-demo.js
@@ -0,0 +1,27 @@
+// See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
+
+var map = L.map( 'map', {
+    center: [-40.0, -72.0],
+    minZoom: 2,
+    
+    zoom: 4
+});
+
+L.tileLayer( 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'OpenStreetMap'
+}).addTo( map );
+
+var geojsonMarkerOptions = {
+    radius: 50,
+    fillColor: "black",
+    color: "#000",
+    weight: 10,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+L.geoJson(geodata, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
