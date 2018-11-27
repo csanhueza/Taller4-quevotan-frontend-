c7af1df8f685f108a44a7541f1f86fb3214d6699
Unknown <lferreira2017@alu.uct.cl>
2018-11-26 21:57:44 -0300

    Modificacion a js que muestra xml

--- Front-end/Sprint 2/Pagina QuienVota2/js/obtener_xml.js
+++ Front-end/Sprint 2/Pagina QuienVota2/js/obtener_xml.js
@@ -1,24 +1,13 @@
-//------------------------
-// Cargar Xml 
-//------------------------
-/*function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-   		myFunction(this);
-    }
-  };
-  xhttp.open("GET", "xml/diputados.xml", true);
-  xhttp.send();
-}
-function myFunction(xml) {
-  var i;
-  var nombre , rut , fecha;
-  var xmlDoc = xml.responseXML;
-  var x = xmlDoc.getElementsByTagName("DiputadoPeriodo");
-  for (i = 0; i <x.length; i++) { 
-   nombre =  x[i].getElementsByTagName("Nombre")[0].childNodes[0].nodeValue.toLowerCase();
-   alert(nombre);
-   }	
-}*/
-
+	var i=0;
+	$(document).ready(function(){
+		$.get('xml/diputados.xml',function(d){
+		$(d).find('DiputadoPeriodo').each(function(){
+			var apellidoP = $(this).find('ApellidoPaterno').text();
+			var apellidM  = $(this).find('ApellidoMaterno').text();
+      var apellidos =apellidoP+" "+apellidM;
+      var menor = 0;
+			var html = "<li class='list-group-item'><a href='#'>"+apellidos +"</a></li>";
+			$("#lista").append($(html));
+			});
+		});
+	});
